b4d2a617a88b2aa6b412cb969a1890714ce5d1bb
Alex <569283998@qq.com>
2015-10-18 20:16:41 +0800

    fix run.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-#from flask.ext.login import LoginManager
-#lm = LoginManager()
-#lm.session_protection = 'strong'
-#lm.init_app(app)
-#lm.login_view = 'index'
+
 from app import views, models
 from config import basedir
 import os
-#from flask.ext.session import Session
-#sess = Session()
-#sess.init_app(app)
 
 
 
